dfc94d889be3416dfd951be71e4b48c31a317578
eyet010 <eyet010@clmns.co.kr>
2022-01-18 17:44:30 +0900

    디테일페이지

--- frontend/src/components/BookDetail.tsx
+++ frontend/src/components/BookDetail.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../redux/hooks/hooks";
+import { SelectBookState } from "../redux/modules/selectBook";
 
 const BookDetail = () => {
-  const search = window.location.search;
-  const params = new URLSearchParams(search);
-  console.log("id ", params);
+  const { id } = useParams();
+  const selectBookState: SelectBookState = useAppSelector(
+    (state) => state.selectBook
+  );
 
   return (
     <div>
-      <h3>Book Detail </h3>
+      <h3>{selectBookState.title} </h3>
     </div>
   );
 };
--- frontend/src/redux/modules/reducer.ts
+++ frontend/src/redux/modules/reducer.ts
@@ -1,17 +1,17 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import { getAuthSaga } from "../saga/authSaga";
 import { getBookSaga } from "../saga/bookSaga";
-import {auth} from "./auth";
+import { auth } from "./auth";
 import book from "./book";
 import menuIndex from "./menuIndex";
+import selectBook from "./selectBook";
 
-const reducer = combineReducers({auth, menuIndex, book});
+const reducer = combineReducers({ auth, menuIndex, book, selectBook });
 
 export function* rootSaga() {
-  yield all([ getBookSaga(), getAuthSaga()]);
+  yield all([getBookSaga(), getAuthSaga()]);
 }
-
 
 export type RootReducerType = ReturnType<typeof reducer>;
 
